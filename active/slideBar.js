c02a8dfc9c1edab61b36ba4941004e639aafd28a
Chase Lose <chasemlose@gmail.com>
2019-07-02 11:50:20 -0400

    SlideBar
    Slide bar has a bug where it disappears.

--- active/slideBar.js
+++ active/slideBar.js
@@ -1,6 +1,12 @@
+/*
+Created By Chase Lose
+*/
 function open() {
-  document.getElementById("slideBar").style.display = "block";
+  document.getElementById("slideBar").style.width = "250px";
+  document.getElementById("area3").style.marginLeft= "250px";
+
 }
 function close() {
-  document.getElementById("slideBar").style.display = "none";
+    document.getElementById("slideBar").style.width = "0px";
+  document.getElementById("area3").style.marginLeft= "0px";
 }
