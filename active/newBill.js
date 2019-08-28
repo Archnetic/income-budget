5e5b887e3876981077fd96702491cba82584f57e
Chase <chasemlose@gmail.com>
2019-08-27 21:18:41 -0400

    Feature Update
    Update for budget side. Now can remove bills and add them in a single line with a click of a button. Remove completely removes the element and leaves nothing behind for cleaner and more robust code.

--- active/newBill.js
+++ active/newBill.js
@@ -1,10 +1,15 @@
 function addBill(){
-    var timesClicked = 3;
-    var add = document.createElement("bill");
-    var orgUnit = "<div id=\"Bill3\">\n" +
-        "\t\t<a href=\"#\">PayMe</a>\n" +
-        "\t</div>";
+    var add = document.createElement("div");
+    add.id = "bill";
+    var orgUnit = "\t\t\t<input type=\"text\" name=\"name\" value=\"fillMe\" class=\"billClass\">\n" +
+        "\t\t\t<input type=\"number\" name=\"amount\" value=\"0\" class=\"billAmountInput\">\n" +
+        "\t\t\t<a href=\"javascript:void(0)\" class=\"deleteBill\" onclick=\"deleteBill()\">&times;</a>\n" +
+        "\t\t</div>";
     add.innerHTML = orgUnit;
-    add.value = "";
     document.getElementById("budgetOrg").appendChild(add);
+};
+
+function deleteBill(){
+    var rem = document.getElementById("bill");
+    rem.parentNode.removeChild(rem);
 };
