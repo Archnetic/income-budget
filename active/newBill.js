02bf1c995b64b30bef9502b8f07db1472e3ab945
Chase <chasemlose@gmail.com>
2019-08-29 10:21:10 -0400

    WIP DO NOT MERGE
    Adding bills add to the bottom category and removing removes the top bill

--- active/newBill.js
+++ active/newBill.js
@@ -1,15 +1,35 @@
+/*
+Created by Chase Lose
+ */
 function addBill(){
-    var add = document.createElement("div");
-    add.id = "bill";
-    var orgUnit = "\t\t\t<input type=\"text\" name=\"name\" value=\"fillMe\" class=\"billClass\">\n" +
-        "\t\t\t<input type=\"number\" name=\"amount\" value=\"0\" class=\"billAmountInput\">\n" +
+    var newBill = document.createElement("div");
+    newBill.id = "bill";
+    var bill = "\t\t\t<input type=\"text\" name=\"name\" value=\"New Bill\" class=\"billClass\">\n" +
+        "\t\t\t<input type=\"number\" name=\"amount\" value=\"0.00\" min=\"0\" step=\"any\" class=\"billAmountInput\">\n" +
+        "\t\t\t<input type=\"date\" name=\"Due Date\" class=\"billDueInput\"> \n" +
         "\t\t\t<a href=\"javascript:void(0)\" class=\"deleteBill\" onclick=\"deleteBill()\">&times;</a>\n" +
         "\t\t</div>";
-    add.innerHTML = orgUnit;
-    document.getElementById("budgetOrg").appendChild(add);
+    newBill.innerHTML = bill;
+    document.getElementById("billCategory").parentNode.appendChild(newBill);
 };
 
 function deleteBill(){
     var rem = document.getElementById("bill");
     rem.parentNode.removeChild(rem);
 };
+
+function newCategory(){
+    var newCat = document.createElement("div");
+    newCat.id = "billCategory";
+    var inject = "\t\t<input type=\"text\" name=\"name\" value=\"New Category\" class=\"categoryInput\">\n" +
+        "<button class=\"addNewBill\" onclick=\"addBill()\">Add New Bill</button>\n" +
+        "<a href=\"javascript:void(0)\" class=\"deleteCat\" onclick=\"deleteCat()\">&times;</a>\n" +
+        "\t</div>\n";
+    newCat.innerHTML = inject;
+    document.getElementById("budgetOrg").appendChild(newCat);
+}
+
+function deleteCat(){
+    var remCat = document.getElementById("billCategory");
+    remCat.parentNode.removeChild(remCat);
+}
