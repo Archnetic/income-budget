7835c462e5f3cae477feaeddda0b16732e4b5609
Chase <chasemlose@gmail.com>
2019-08-30 11:22:53 -0400

    WIP bug fix
    Deleting now deletes the proper ones

--- active/newBill.js
+++ active/newBill.js
@@ -1,21 +1,22 @@
 /*
 Created by Chase Lose
  */
+var timesPressed = 0;
 function addBill(){
+    timesPressed++;
     var newBill = document.createElement("div");
-    newBill.id = "bill";
+    newBill.id = "bill" + timesPressed;
     var bill = "\t\t\t<input type=\"text\" name=\"name\" value=\"New Bill\" class=\"billClass\">\n" +
         "\t\t\t<input type=\"number\" name=\"amount\" value=\"0.00\" min=\"0\" step=\"any\" class=\"billAmountInput\">\n" +
         "\t\t\t<input type=\"date\" name=\"Due Date\" class=\"billDueInput\"> \n" +
-        "\t\t\t<a href=\"javascript:void(0)\" class=\"deleteBill\" onclick=\"deleteBill()\">&times;</a>\n" +
+        "\t\t\t<a href=\"javascript:void(0)\" class=\"deleteBill\" onclick=\"deleteBill(this)\">&times;</a>\n" +
         "\t\t</div>";
     newBill.innerHTML = bill;
     document.getElementById("billCategory").parentNode.appendChild(newBill);
 };
 
-function deleteBill(){
-    var rem = document.getElementById("bill");
-    rem.parentNode.removeChild(rem);
+function deleteBill(rem){
+    rem.parentNode.remove();
 };
 
 function newCategory(){
@@ -23,13 +24,12 @@ function newCategory(){
     newCat.id = "billCategory";
     var inject = "\t\t<input type=\"text\" name=\"name\" value=\"New Category\" class=\"categoryInput\">\n" +
         "<button class=\"addNewBill\" onclick=\"addBill()\">Add New Bill</button>\n" +
-        "<a href=\"javascript:void(0)\" class=\"deleteCat\" onclick=\"deleteCat()\">&times;</a>\n" +
+        "<a href=\"javascript:void(0)\" class=\"deleteCat\" onclick=\"deleteCat(this)\">&times;</a>\n" +
         "\t</div>\n";
     newCat.innerHTML = inject;
     document.getElementById("budgetOrg").appendChild(newCat);
 }
 
-function deleteCat(){
-    var remCat = document.getElementById("billCategory");
-    remCat.parentNode.removeChild(remCat);
+function deleteCat(remCat){
+    remCat.parentNode.remove();
 }
